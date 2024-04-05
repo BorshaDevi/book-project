66d86ab2533a67a6f63ddd0cadf3500ca0452fbd
Borshadevi <borshadevi27@gmail.com>
2024-04-05 21:49:18 +0600

    add root file

---
+++ src/Components/Error/Error.jsx
@@ -0,0 +1,11 @@
+
+
+const Error = () => {
+    return (
+        <div>
+            <h1>oops!</h1>
+        </div>
+    );
+};
+
+export default Error;
---
+++ src/Components/Root/Root.jsx
@@ -0,0 +1,14 @@
+import Header from "../Header/Header";
+
+
+const Root = () => {
+    return (
+        <div>
+            <Header></Header>
+            
+            
+        </div>
+    );
+};
+
+export default Root;
